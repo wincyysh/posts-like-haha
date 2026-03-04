97d5ea163aca365441f756b5dcd2b0c54c0b92e7
wincyysh <141292574+wincyysh@users.noreply.github.com>
2026-03-04 01:53:10 +0100

    creating a post

--- frontend/src/components/ContentCreator.jsx
+++ frontend/src/components/ContentCreator.jsx
@@ -7,6 +7,8 @@ const ContentCreator = () => {
     const [imagePreview, setImagePreview] = useState(null);
 
     const handleImage = (e)=>{
+        e.preventDefault();
+        
         const file = e.target.files[0];
         if(file){
             setImage(file);
@@ -14,8 +16,25 @@ const ContentCreator = () => {
         }
     };
 
-    const handleSubmit = ()=>{
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(!content.trim()){
+            alert('Enter some content!');
+            return;
+        }
+        
+        const postData ={
+            content,
+            authorName: 'Test User',
+            authorId: '001',
+            image
+        };
 
+        try{
+            
+        }catch(error){
+            alert(error)
+        }
     };
 
 
@@ -32,8 +51,7 @@ const ContentCreator = () => {
                     name="story" 
                     rows="5" 
                     cols="33">
-                </textarea>                    
-
+                </textarea>                   
                 <label>
                     Choose image to upload
                     <input 
@@ -47,13 +65,14 @@ const ContentCreator = () => {
                     (<div>
                         <img src={imagePreview} />
                         <button
-                            type="buttom"
+                            type="button"
                             onClick={()=>{setImage(null); setImagePreview(null);}}
                         >
                             Remove your image
                         </button>
                     </div>)
                 }
+                <button type="submit">submit</button>
             </form>
         </div>
     );
