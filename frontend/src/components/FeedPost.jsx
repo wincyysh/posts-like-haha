244314758ea89c2dcd762ca63913622f87276497
wincyysh <141292574+wincyysh@users.noreply.github.com>
2026-03-15 20:14:58 +0100

    post date

--- frontend/src/components/FeedPost.jsx
+++ frontend/src/components/FeedPost.jsx
@@ -10,7 +10,6 @@ const FeedPost = () => {
 
     const dispatch = useDispatch();
     const { posts, status, error } = useSelector((state) => { state.feed });
-
     useEffect( () => { dispatch(fetchPosts()); }, [dispatch]);
     
     const handleReaction = (postId, reactionType) => {
@@ -23,7 +22,43 @@ const FeedPost = () => {
         }
     };
 
-    
+    const formateDate = (dateString) => {
+        const date = new Date(dateString);
+        const now = new Date();
+        const diffMs = now - date;
+        const diffMins = mathfloor(diffMs / 60000);
+        const diffHours = Math.floor(diffMs / 3600000);
+        const diffDays = Math.floor(diffMs / 86400000);
+
+        if (diffMins < 1) return 'Just Now';
+        if (diffMins < 1) return `${diffMins}m ago`;
+        if (diffHours < 24) return `${diffHours}h ago`;
+        if (diffDays < 7) return `${diffDays}d ago`; 
+
+        return date.toLocaleDateString();
+    }
+
+    if (status === 'loading' && posts.length === 0){
+        return (
+            <div className="loading-container">
+                <h3>Loading posts ... </h3>
+            </div>
+        );
+    }
+    if (status === 'failed') {
+        return (
+            <div className="error-container">
+                <h3> Error Loading posts </h3>
+                <p>{error}</p>
+                <button
+                    onClick={() => dispatch(fetchPosts())}
+                    className="retry-buttom"
+                >
+                    retry
+                </button>
+            </div>
+        );
+    }
 };
 
 export default FeedPost;
